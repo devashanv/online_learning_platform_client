cdf254ddc11c45013a2775a71bc1d5c03acbdd41
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2025-01-13 17:28:59 +0530

    adds dashboard UI to the application

---
+++ src/pages/common/AllCourses.jsx
@@ -0,0 +1,191 @@
+import React from 'react'
+
+const AllCourses = (prop) => {
+  return (
+    <>
+      {prop.status === "open" ? (
+        <>
+          {/* search sec*/}
+          <div className="w-full px-4 py-10 sm:p-20 lg:p-20">
+            <form className="w-full gap-1 lg:gap-1 lg:w-3/5 h-10 sm:h-12 lg:h-12 mx-auto flex justify-between">
+              <input
+                type="text"
+                placeholder="search here.."
+                className="w-5/6 h-full pl-4 text-xs lg:text-base rounded-lg border-2 hover:ring-2 hover:ring-btn-color focus:ring-btn-color focus:outline-none"
+              />
+
+              <button
+                type="submit"
+                className="bg-btn-color text-xs sm:text-base lg:text-base text-secondary hover:bg-btn-color/[0.9] w-32 rounded-lg"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+
+          {/* course sec */}
+          <div className="h-auto">
+            <div className=" mx-auto flex flex-wrap justify-between">
+              <div className="bg-secondary text-primary  shadow-card rounded-lg w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+            </div>
+          </div>        
+        </>
+      ) : (
+        <>
+          {/* search sec*/}
+          <div className="w-full px-4 py-10 sm:p-20 lg:p-20">
+            <form className="w-full gap-1 lg:gap-0 lg:w-3/5 h-10 sm:h-12 lg:h-12 mx-auto flex justify-between">
+              <input
+                type="text"
+                placeholder="search here.."
+                className="w-5/6 h-full pl-4 text-xs lg:text-base rounded-lg border-2 hover:ring-2 hover:ring-btn-color focus:ring-btn-color focus:outline-none"
+              />
+
+              <button
+                type="submit"
+                className="bg-btn-color text-xs sm:text-base lg:text-base text-secondary hover:bg-btn-color/[0.9] w-32 rounded-lg"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+
+          {/* courses sec */}
+          <div className="h-auto w-4/5 mx-auto">
+            <div className=" mx-auto flex flex-wrap justify-between">
+              {/* card */}
+              <div className="bg-secondary text-primary  shadow-card rounded-lg sm:w-64 lg:w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+              <div className="bg-secondary text-primary  shadow-card rounded-lg sm:w-64 lg:w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+              <div className="bg-secondary text-primary  shadow-card rounded-lg sm:w-64 lg:w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+              <div className="bg-secondary text-primary  shadow-card rounded-lg sm:w-64 lg:w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+              <div className="bg-secondary text-primary  shadow-card rounded-lg sm:w-64 lg:w-72 mt-5 min-h-52 flex flex-col p-3">
+                <header className="h-2/6 flex flex-col gap-1">
+                  <h2 className="text-center text-base font-semibold">
+                    Introduction to Web Development
+                  </h2>
+                  <p className="text-xs font-normal text-background">
+                    Author:
+                    <span>Jhon Doe</span>
+                  </p>
+                  <hr />
+                </header>
+
+                <p className="h-3/6 text-xs mt-5">
+                  Learn the basics of building websites, including HTML, CSS, and
+                  JavaScript.
+                </p>
+
+                <button className="w-full border-2 border-btn-color rounded-lg text-primary text-sm py-1 self-end mt-4 hover:bg-btn-color/[0.5]">
+                  More
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+  
+    </>
+  );
+}
+
+export default AllCourses
